f7bbb40fa1ca264f31ce3bc9b1d1827913f302fa
Samba <samba.pendela@gmail.com>
2021-07-30 21:54:10 +0530

    keywords started

--- modifiers.js
+++ modifiers.js
@@ -1,45 +1,5 @@
 //public
 //private
 //protected
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-//public
-//public modifier applicable to "variables", "functions", "constructor"
-//public modifier not applicable to "classes"
-//public members are accessable by class "objects"
-//public members accessable to "child" classes also
-/*
-    public class class_one{}
-    //'public' modifier cannot appear on a module or namespace element.
-*/
-var class_one = /** @class */ (function () {
-    function class_one() {
-        this.var_one = "Hello_1";
-    }
-    class_one.prototype.fun_one = function () {
-        return this.var_one;
-    };
-    return class_one;
-}());
-var class_two = /** @class */ (function (_super) {
-    __extends(class_two, _super);
-    function class_two() {
-        return _super !== null && _super.apply(this, arguments) || this;
-    }
-    return class_two;
-}(class_one));
-var obj = new class_two();
-console.log(obj.fun_one());
+var var_one = "Hello";
+console.log(var_one);
--- modifiers.ts
+++ modifiers.ts
@@ -79,6 +79,214 @@
 
 
 
+//protected
+//protected members available to "child" classes
+//protected members, we can't access by using class "objects"
+//protected applicable to variables, functions, constructor
+//protected won't applicable to class level
+
+/*
+    protected class class_one{}
+    //'protected' modifier cannot appear on a module or namespace element.
+*/
+
+
+/*
+    class class_one{
+        protected constructor(){}
+    }
+    new class_one();        
+    //Constructor of class 'class_one' is protected and only accessible within the class declaration.
+*/
+
+
+
+/*
+    class class_one{
+        protected var_one:string = "Hello";
+        protected fun_one():string{
+            return "Hello";
+        }
+    }
+    let obj:class_one = new class_one();
+    obj.var_one;
+    obj.fun_one();
+*/
+
+
+/*
+    class class_one{
+        protected var_one:string = "Hello";
+    };
+
+    class class_two extends class_one{
+
+    };
+    let obj:class_two = new class_two();
+    obj.var_one;
+    //Property 'var_one' is protected and only accessible within class 'class_one' and its subclasses.
+*/
+
+
+/*
+    class class_one{
+        protected var_one:string = "Hello_1";
+        protected fun_one():string{
+            return "Hello_2";
+        }
+    }
+    class class_two extends class_one{
+        var_two:string = this.var_one;
+        fun_two():string{
+            return this.fun_one();
+        };
+    }
+    let obj:class_two = new class_two();
+    console.log(
+        obj.var_two,
+        obj.fun_two()
+    );   //Hello_1 Hello_2
+*/
+
+
+//1) this
+//2) super
+//3) static
+//4) readonly
+//5) type
+
+
+
+//type
+type my_name = string;
+let var_one:my_name = "Hello";
+console.log( var_one );             //Hello
+
+
+type x = string | number | boolean;
+function fun_one(arg1:x):void{
+    console.log( arg1 );
+}
+fun_one("Hello");
+fun_one(100);
+fun_one(true);
+//fun_one([]);
+//Argument of type 'undefined[]' is not assignable to parameter of type 'x'.
+
+
+type subjects = "Angular12" | "NodeJS" | "ReactJS" | "VueJS" | "MongoDB";
+function fun_two(sub:subjects):void{
+    console.log( sub );
+}
+fun_two("Angular12");
+fun_two("MongoDB");
+//fun_two("Cyber Secuirity");
+//Argument of type '"Cyber Secuirity"' is not assignable to parameter of type 'subjects'.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
